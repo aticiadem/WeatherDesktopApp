0eae1777e12e4ce77b0fbc616f17f7993e1a5967
AdemAtici <adematicis41@gmail.com>
2024-02-10 16:33:32 +0300

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,13 @@ repositories {
 }
 
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
+    val ktor = "2.3.8"
+
     implementation(compose.desktop.currentOs)
+    implementation("io.ktor:ktor-client-core:$ktor")
+    implementation("io.ktor:ktor-client-cio:$ktor")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor")
+    implementation("io.ktor:ktor-serialization-gson:$ktor")
 }
 
 compose.desktop {
