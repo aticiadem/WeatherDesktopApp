d305b786d671e73d0600b45ef1540e47abd7461d
AdemAtici <adematicis41@gmail.com>
2024-02-10 22:36:18 +0300

    Update Constants

--- src/main/kotlin/util/Constants.kt
+++
@@ -1,6 +0,0 @@
-package util
-
-object Constants {
-    val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    val API_KEY = "Your Api Key"
-}
---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,10 @@
+package utils
+
+import androidx.compose.ui.graphics.Color
+
+object Constants {
+    val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    val API_KEY = "YOUR API KEY"
+    val APP_NAME = "Weather App"
+    val BACKGROUND_COLOR = Color(0xFFADD8E6)
+}
